b70a014c8394c987ed28ef5a546d5ee3da84e9fd
glindberg <gustav.lindberg1@gmail.com>
2019-03-27 15:26:03 +0100

    Added API key

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: 'AIzaSyCFUB3j_pjMoh5L0ncPXXUSBAsDuDgYhGg',
+  authDomain: 'react-firebase-authentic-2741c.firebaseapp.com',
+  databaseURL:
+    'https://react-firebase-authentic-2741c.firebaseio.com',
+  projectId: 'react-firebase-authentic-2741c',
+  storageBucket: 'react-firebase-authentic-2741c.appspot.com',
+  messagingSenderId: '843373606729',
 };
 
 class Firebase {
